74f56b1d3b1923f50db3cc9498b3ca3aa2532ad5
Hitesh Tiwari <hiteshtiwari1443@gmail.com>
2021-05-21 09:41:34 +0530

    css loader

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Wrap from "../../hoc/Wrap";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Wrap>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Wrap>
+    );
+  }
+}
+
+export default BurgerBuilder;
